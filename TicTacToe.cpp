4257fabc31fe97a2b326732907d87cd978524e68
ApBaidya <aparajitabaidya2@gmail.com>
2025-10-02 21:57:49 +0100

    planned out the code

---
+++ TicTacToe.cpp
@@ -0,0 +1,34 @@
+/*
+Aparajita Baidya
+10.2.2025
+Tic-Tac-Toe
+Create a 3x3 grid (letters and nums) --- 2d array
+User enters move
+Check if move is legal
+Enter if move is legal, if not, ask again
+check if win or tie --> tell users
+Reset board, inform players of currents wins and ties
+ask if replay(?) -- redraw board
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//declare functions
+
+
+//main
+//while gameon == true, run game
+//when board full, end game, ask if play again, if 'n', gameon == false
+//make sure to get the user input properly 
+//reset board at top 
+
+
+//check move legality --> return bool   
+
+
+//check board state --> return char --> if board filled: 'x', 'o', 'n'
+
+
+//score keeper --> return char array --> ['x',':','#',' ','o',':','#',' ','t','i','e',':','#']
